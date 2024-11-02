b781dbea72607ccc7082a0d9f637fb1491a285b5
CarlosQ <cquiroag2@miumg.edu.gt>
2024-11-01 20:30:25 -0600

    recompilando con tailwind nueva versión y correxiones varias

--- frontend/src/components/AddProjectModal.js
+++ frontend/src/components/AddProjectModal.js
@@ -1,68 +1,66 @@
-import React, { Fragment, memo, useEffect, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import BtnPrimary from './BtnPrimary'
-import BtnSecondary from './BtnSecondary'
-import axios from "axios"
-import toast from 'react-hot-toast'
+import React, { Fragment, memo, useEffect, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import BtnPrimary from './BtnPrimary';
+import BtnSecondary from './BtnSecondary';
+import axios from "axios";
+import toast from 'react-hot-toast';
 
 const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) => {
-
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
     const [desc, setDesc] = useState('');
 
     useEffect(() => {
         if (edit && isModalOpen) {
             axios.get(`http://localhost:9000/project/${id}`)
                 .then((res) => {
-                    setTitle(res.data[0].title)
-                    setDesc(res.data[0].description)
+                    setTitle(res.data[0].title);
+                    setDesc(res.data[0].description);
                 })
                 .catch((error) => {
-                    toast.error('Something went wrong')
-                })
+                    console.error(error);
+                    toast.error('Something went wrong');
+                });
         }
-    }, [isModalOpen]);
-
+    }, [edit, id, isModalOpen]); // Incluye 'edit' e 'id' en el array de dependencias
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         if (!edit) {
             axios.post('http://localhost:9000/project/', { title, description: desc })
                 .then((res) => {
-                    closeModal()
+                    closeModal();
                     const customEvent = new CustomEvent('projectUpdate', { detail: { ...res.data } });
                     document.dispatchEvent(customEvent);
-                    toast.success('Project created successfully')
-                    setTitle('')
-                    setDesc('')
+                    toast.success('Project created successfully');
+                    setTitle('');
+                    setDesc('');
                 })
                 .catch((error) => {
                     if (error.response.status === 422) {
-                        toast.error(error.response.data.details[0].message)
+                        toast.error(error.response.data.details[0].message);
                     } else {
-                        toast.error('Something went wrong')
+                        toast.error('Something went wrong');
                     }
-                })
+                });
         } else {
             axios.put(`http://localhost:9000/project/${id}`, { title, description: desc })
                 .then((res) => {
-                    closeModal()
+                    closeModal();
                     const customEvent = new CustomEvent('projectUpdate', { detail: { ...res.data } });
                     document.dispatchEvent(customEvent);
-                    toast.success('Project updated successfully')
-                    setTitle('')
-                    setDesc('')
+                    toast.success('Project updated successfully');
+                    setTitle('');
+                    setDesc('');
                 })
                 .catch((error) => {
                     if (error.response.status === 422) {
-                        toast.error(error.response.data.details[0].message)
+                        toast.error(error.response.data.details[0].message);
                     } else {
-                        toast.error('Something went wrong')
+                        toast.error('Something went wrong');
                     }
-                })
+                });
         }
-
-    }
+    };
 
     return (
         <Transition appear show={isModalOpen} as={Fragment}>
@@ -80,10 +78,9 @@ const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) =
                         <div className="fixed inset-0 bg-black/30" />
                     </Transition.Child>
                     <div className="fixed inset-0 flex items-center justify-center p-4 w-screen h-screen ">
-                        {/* <div className="fixed inset-0 "> */}
                         <Transition.Child
                             as={Fragment}
-                            enter="ease-out duration-300 "
+                            enter="ease-out duration-300"
                             enterFrom="opacity-0 scale-95"
                             enterTo="opacity-100 scale-100"
                             leave="ease-in duration-200"
@@ -91,11 +88,10 @@ const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) =
                             leaveTo="opacity-0 scale-95"
                         >
                             <Dialog.Panel className="rounded-md bg-white w-6/12">
-
                                 <Dialog.Title as='div' className={'bg-white shadow px-6 py-4 rounded-t-md sticky top-0'}>
                                     {edit ? (<h1>Edit Project</h1>) : (<h1>Create Project</h1>)}
-                                    <button onClick={() => closeModal()} className=' absolute right-6 top-4 text-gray-500 hover:bg-gray-100 rounded focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-200 '>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className=" w-6 h-6">
+                                    <button onClick={() => closeModal()} className='absolute right-6 top-4 text-gray-500 hover:bg-gray-100 rounded focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-200'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
                                         </svg>
                                     </button>
@@ -114,15 +110,13 @@ const AddProjectModal = ({ isModalOpen, closeModal, edit = false, id = null }) =
                                         <BtnPrimary>Save</BtnPrimary>
                                     </div>
                                 </form>
-
                             </Dialog.Panel>
                         </Transition.Child>
-
                     </div>
                 </div>
             </Dialog>
         </Transition>
-    )
-}
+    );
+};
 
-export default memo(AddProjectModal)
+export default memo(AddProjectModal);
--- frontend/src/components/AddTaskModal.js
+++ frontend/src/components/AddTaskModal.js
@@ -1,64 +1,63 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import BtnPrimary from './BtnPrimary'
-import BtnSecondary from './BtnSecondary'
-import axios from 'axios'
-import toast from 'react-hot-toast'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import BtnPrimary from './BtnPrimary';
+import BtnSecondary from './BtnSecondary';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const AddTaskModal = ({ isAddTaskModalOpen, setAddTaskModal, projectId = null, taskId = null, edit = false, refreshData }) => {
-
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
     const [desc, setDesc] = useState('');
 
     useEffect(() => {
         if (edit && isAddTaskModalOpen) {
             axios.get(`http://localhost:9000/project/${projectId}/task/${taskId}`)
                 .then((res) => {
-                    setTitle(res.data[0].task[0].title)
-                    setDesc(res.data[0].task[0].description)
+                    setTitle(res.data[0].task[0].title);
+                    setDesc(res.data[0].task[0].description);
                 })
                 .catch((error) => {
-                    toast.error('Something went wrong')
-                })
-            console.log('edit function call')
+                    toast.error('Something went wrong');
+                });
+            console.log('edit function call');
         }
-    }, [isAddTaskModalOpen]);
+    }, [edit, projectId, taskId, isAddTaskModalOpen]); // Incluye 'edit', 'projectId' y 'taskId' en el array de dependencias
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         if (!edit) {
             axios.post(`http://localhost:9000/project/${projectId}/task`, { title, description: desc })
                 .then((res) => {
-                    setAddTaskModal(false)
-                    toast.success('Task created successfully')
-                    setTitle('')
-                    setDesc('')
+                    setAddTaskModal(false);
+                    toast.success('Task created successfully');
+                    setTitle('');
+                    setDesc('');
                 })
                 .catch((error) => {
                     if (error.response.status === 422) {
-                        toast.error(error.response.data.details[0].message)
+                        toast.error(error.response.data.details[0].message);
                     } else {
-                        toast.error('Something went wrong')
+                        toast.error('Something went wrong');
                     }
-                })
+                });
         } else {
             axios.put(`http://localhost:9000/project/${projectId}/task/${taskId}`, { title, description: desc })
                 .then((res) => {
-                    setAddTaskModal(false)
-                    toast.success('Task is updated')
-                    refreshData(true)
-                    setTitle('')
-                    setDesc('')
+                    setAddTaskModal(false);
+                    toast.success('Task is updated');
+                    refreshData(true);
+                    setTitle('');
+                    setDesc('');
                 })
                 .catch((error) => {
                     if (error.response.status === 422) {
-                        toast.error(error.response.data.details[0].message)
+                        toast.error(error.response.data.details[0].message);
                     } else {
-                        toast.error('Something went wrong')
+                        toast.error('Something went wrong');
                     }
-                })
+                });
         }
-    }
+    };
 
     return (
         <Transition appear show={isAddTaskModalOpen} as={Fragment}>
@@ -76,7 +75,6 @@ const AddTaskModal = ({ isAddTaskModalOpen, setAddTaskModal, projectId = null, t
                         <div className="fixed inset-0 bg-black/30" />
                     </Transition.Child>
                     <div className="fixed inset-0 flex items-center justify-center p-4 w-screen h-screen">
-                        {/* <div className="fixed inset-0 "> */}
                         <Transition.Child
                             as={Fragment}
                             enter="ease-out duration-300"
@@ -87,10 +85,9 @@ const AddTaskModal = ({ isAddTaskModalOpen, setAddTaskModal, projectId = null, t
                             leaveTo="opacity-0 scale-95"
                         >
                             <Dialog.Panel className="rounded-md bg-white w-6/12">
-
                                 <Dialog.Title as='div' className={'bg-white shadow px-6 py-4 rounded-t-md sticky top-0'}>
                                     {!edit ? (<h1>Add Task</h1>) : (<h1>Edit Task</h1>)}
-                                    <button onClick={() => setAddTaskModal(false)} className='absolute right-6 top-4 text-gray-500 hover:bg-gray-100 rounded focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-200 '>
+                                    <button onClick={() => setAddTaskModal(false)} className='absolute right-6 top-4 text-gray-500 hover:bg-gray-100 rounded focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-200'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                             <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
                                         </svg>
@@ -110,15 +107,13 @@ const AddTaskModal = ({ isAddTaskModalOpen, setAddTaskModal, projectId = null, t
                                         <BtnPrimary>Save</BtnPrimary>
                                     </div>
                                 </form>
-
                             </Dialog.Panel>
                         </Transition.Child>
-
                     </div>
                 </div>
             </Dialog>
         </Transition>
-    )
-}
+    );
+};
 
-export default AddTaskModal
+export default AddTaskModal;
